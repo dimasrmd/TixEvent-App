0648d85ba74923528aa2db021c7d7f40768b94f8
Abisar Fathir <abisarfathir7@gmail.com>
2026-05-28 21:00:37 +0700

    Update TenantRepository.java

--- backend/src/main/java/com/tixevent/backend/repository/TenantRepository.java
+++ backend/src/main/java/com/tixevent/backend/repository/TenantRepository.java
@@ -2,8 +2,5 @@ package com.tixevent.backend.repository;
 
 import com.tixevent.backend.entity.Tenant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface TenantRepository extends JpaRepository<Tenant, String> {
 }
