a187fe67ff6b5b223d56c2b7974e799ac80451ee
Zackjek <zakymubarok563@gmail.com>
2026-05-29 14:00:43 +0700

    feat: migrasi JPA entity, relasi database, dan setup repository, tambahan resolve repository

--- backend/src/main/java/com/tixevent/backend/repository/TiketRepository.java
+++ backend/src/main/java/com/tixevent/backend/repository/TiketRepository.java
@@ -2,8 +2,6 @@ package com.tixevent.backend.repository;
 
 import com.tixevent.backend.entity.Tiket; // Sesuaikan dengan entity
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TiketRepository extends JpaRepository<Tiket, String> {
 }
--- backend/src/main/java/com/tixevent/backend/repository/TransaksiRepository.java
+++ backend/src/main/java/com/tixevent/backend/repository/TransaksiRepository.java
@@ -2,8 +2,6 @@ package com.tixevent.backend.repository;
 
 import com.tixevent.backend.entity.Transaksi; // Sesuaikan dengan entity
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TransaksiRepository extends JpaRepository<Transaksi, String> {
 }
