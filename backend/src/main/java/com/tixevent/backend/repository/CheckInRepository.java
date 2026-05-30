a98e1ee90f64272a27f15cec820cc57898ee22a4
Ariel Ahnaf Kusuma <ahnaf2406@gmail.com>
2026-05-30 22:57:42 +0700

    fix: hapus anotasi @Repository dan import yang tidak diperlukan sesuai revisi

--- backend/src/main/java/com/tixevent/backend/repository/CheckInRepository.java
+++ backend/src/main/java/com/tixevent/backend/repository/CheckInRepository.java
@@ -2,9 +2,7 @@ package com.tixevent.backend.repository;
 
 import com.tixevent.backend.entity.CheckInSystem;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CheckInRepository extends JpaRepository<CheckInSystem, String> {
     // Interface ini murni dikosongkan karena sudah otomatis mewarisi seluruh fungsi CRUD database.
     // Parameter pertama 'CheckInSystem' adalah kelas entitas yang ditargetkan.
